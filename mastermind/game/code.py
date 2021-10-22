bc99122e02aa14ab280f222687644f359be5a180
Jennifer Clark <jennifer.c.schultz@gmail.com>
2021-10-21 23:27:47 -0600

    added comment to code.py to see if team-test is updatingcorrectly

--- mastermind/game/code.py
+++ mastermind/game/code.py
@@ -1,7 +1,7 @@
 
 import random
  #this should generate a secret random number between 1000 and 9999
- #this is the number the players are trying to guess. 
+ #this is the number the players are trying to guess.  this is already done in board though. 
  
 class Code:
 
